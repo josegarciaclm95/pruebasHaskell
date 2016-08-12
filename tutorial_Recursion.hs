d71737a28af913a8ddc2349b80b0e736bd57e27a
Jose Maria <josemariagarcia95@gmail.com>
2016-08-12 19:03:25 +0200

    Continuacion HS. Entrada Salida

--- tutorial_Recursion.hs
+++ tutorial_Recursion.hs
@@ -1,4 +1,6 @@
---Tutorial recursion 
+--Tutorial recursion
+import Data.Char
+import Data.List
 
 repeat' :: a -> [a]
 repeat' x = x : repeat' x
@@ -12,3 +14,21 @@ quicksort (x:xs) =
 
 multThree :: (Num a) => a -> a -> a -> a
 multThree x y z = x * y * z
+
+orderWeight :: String -> String
+orderWeight [] = []
+orderWeight ys = unwords [b | (a,b) <- (toIntv2 $ words ys)]
+
+toInt :: [Char] -> [Int]
+toInt = foldl (\a b ->a ++ [digitToInt b]) []
+
+toIntv2 :: [[Char]] -> [(Int, String)]
+toIntv2 xs = sortBy (\(a,b) (c,d) -> if a == c then compare b d else compare a c) [(sum $ toInt x, x)| x <- xs]
+
+{--No hace falta poner read x::Int porque en la cabecera de la funcion ya hay informacion como para que read sepa que hacer
+toInt :: String -> Int
+toInt = read
+
+toInt' :: [String] -> [([Int],String)]
+toInt' xs = [([digitToInt y | y <- x],x) | x <- xs]
+--}
